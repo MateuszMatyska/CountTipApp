20ecbcaafdad3292b5b6a8fdd6f5b6e51ab8270b
Mateusz Matyska <mateuszmatyska@gmail.com>
2024-05-24 00:23:51 +0200

    feat: Add Model and ViewModel for Bills

---
+++ app/src/main/java/com/example/counttipapp/data/Bill.kt
@@ -0,0 +1,8 @@
+package com.example.counttipapp.data
+
+data class Bill(
+    val cost: Double = 0.0,
+    val tipPercentage: Double = 0.0,
+    val tip: Double = 0.0,
+    val result: Double = 0.0,
+)
