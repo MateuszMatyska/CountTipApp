d37a1b631604145ccd5c7b3967df043a442fe1b8
Mateusz Matyska <mateuszmatyska@gmail.com>
2024-05-31 17:50:13 +0200

    feat: Add Slider component

--- app/src/main/java/com/example/counttipapp/MainActivity.kt
+++ app/src/main/java/com/example/counttipapp/MainActivity.kt
@@ -5,21 +5,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import com.example.counttipapp.components.TipButton
 import com.example.counttipapp.components.TipInput
 import com.example.counttipapp.components.TipLabel
+import com.example.counttipapp.components.TipSlider
 import com.example.counttipapp.ui.theme.CountTipAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,6 +30,7 @@ class MainActivity : ComponentActivity() {
                         TipButton(title = "Title", onPressAction = { }, enabled = true)
                         TipLabel(title = "Title", text = "Text")
                         TipInput(value = "", placeholder = "Placeholder", onNameChange = {})
+                        TipSlider(value = 10f, onChange = {})
                     }
                 }
             }
--- app/src/main/java/com/example/counttipapp/components/TipSlider.kt
+++ app/src/main/java/com/example/counttipapp/components/TipSlider.kt
@@ -1,8 +1,10 @@
 package com.example.counttipapp.components
 
+import androidx.compose.material3.Slider
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
-fun TipSlider() {
-
+fun TipSlider(value: Float, onChange: (Float) -> Unit, modifier: Modifier = Modifier) {
+    Slider(value = value, onValueChange = onChange, valueRange = 0f..100f, steps = 9, modifier = modifier)
 }
