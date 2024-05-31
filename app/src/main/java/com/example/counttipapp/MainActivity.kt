ea935ab29ba929a7b302943eb4ae1e5c1f9a1c2b
Mateusz Matyska <mateuszmatyska@gmail.com>
2024-05-31 18:51:49 +0200

    feat: Add Application title, update theme

--- app/src/main/java/com/example/counttipapp/MainActivity.kt
+++ app/src/main/java/com/example/counttipapp/MainActivity.kt
@@ -1,39 +1,17 @@
 package com.example.counttipapp
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import com.example.counttipapp.components.TipButton
-import com.example.counttipapp.components.TipInput
-import com.example.counttipapp.components.TipLabel
-import com.example.counttipapp.components.TipSlider
-import com.example.counttipapp.ui.theme.CountTipAppTheme
+import com.example.counttipapp.compose.TipScreen
 
 class MainActivity : ComponentActivity() {
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CountTipAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) {
-                    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
-                        TipButton(title = "Title", onPressAction = { }, enabled = true)
-                        TipLabel(title = "Title", text = "Text")
-                        TipInput(value = "", placeholder = "Placeholder", onNameChange = {})
-                        TipSlider(value = 10f, onChange = {})
-                    }
-                }
-            }
+            TipScreen()
         }
     }
 }
